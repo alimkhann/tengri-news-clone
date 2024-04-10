8984e638b799b1b8b7cf2bf4b4fc1c770f3e9ca8
alimkhann <alimkhan.ergebayev@gmail.com>
2024-04-10 22:16:55 +0500

    sort latest and popular news

--- app/js/script.js
+++ app/js/script.js
@@ -27,28 +27,53 @@ document.addEventListener("DOMContentLoaded", function () {
         gridViewContainer.innerHTML += card;
       });
 
-      const scrollViewContainer = document.querySelector(".scroll-news__list");
-      data.scrollView.forEach((item) => {
-        const sideCard = `
-                    <div class="side-card">
-                        <span class="side-card__title">
-                            <a href="${item.articleUrl}">
-                                ${item.title}
-                            </a>
-                        </span>
-                        <div class="side-card__meta">
-                            ${item.time}
-                            <div class="side-card__meta__vc">
-                                <img src="images/svgs/viewings.svg">
-                                ${item.views}
-                                <img class="side-card__meta--comment" src="images/svgs/comments.svg">
-                                ${item.comments}
-                            </div>
-                        </div>
-                    </div>
-                `;
-        scrollViewContainer.innerHTML += sideCard;
+      const renderArticles = (articles) => {
+        const scrollViewContainer =
+          document.querySelector(".scroll-news__list");
+        scrollViewContainer.innerHTML = "";
+        articles.forEach((item) => {
+          const sideCard = `
+              <div class="side-card">
+                  <span class="side-card__title">
+                      <a href="${item.articleUrl}">
+                          ${item.title}
+                      </a>
+                  </span>
+                  <div class="side-card__meta">
+                      ${item.time}
+                      <div class="side-card__meta__vc">
+                          <img src="images/svgs/viewings.svg">
+                          ${item.views}
+                          <img class="side-card__meta--comment" src="images/svgs/comments.svg">
+                          ${item.comments}
+                      </div>
+                  </div>
+              </div>
+          `;
+          scrollViewContainer.innerHTML += sideCard;
+        });
+      };
+
+      const radioButtons = document.querySelectorAll(
+        'input[name="side-button"]'
+      );
+
+      function sortArticlesByViews(articles) {
+        return articles.slice().sort((a, b) => b.views - a.views);
+      }
+
+      radioButtons.forEach((button, index) => {
+        button.addEventListener("change", () => {
+          if (index === 1) {
+            const sortedArticles = sortArticlesByViews(data.scrollView);
+            renderArticles(sortedArticles);
+          } else {
+            renderArticles(data.scrollView);
+          }
+        });
       });
+
+      renderArticles(data.scrollView);
     })
     .catch((error) => console.error("Error fetching data:", error));
 });
